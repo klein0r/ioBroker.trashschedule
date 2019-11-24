eaa93dd19ceda4d5639a4587beab6f94698b5133
matthias@klein0r.de <Matthias Kleine>
2019-11-24 17:02:49 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'trashschedule adapter settings': {
+        'en': 'Adapter settings for trashschedule',
+        'de': 'Adaptereinstellungen für trashschedule',
+        'ru': 'Настройки адаптера для trashschedule',
+        'pt': 'Configurações do adaptador para trashschedule',
+        'nl': 'Adapterinstellingen voor trashschedule',
+        'fr': "Paramètres d'adaptateur pour trashschedule",
+        'it': "Impostazioni dell'adattatore per trashschedule",
+        'es': 'Ajustes del adaptador para trashschedule',
+        'pl': 'Ustawienia adaptera dla trashschedule',
+        'zh-cn': 'trashschedule的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
