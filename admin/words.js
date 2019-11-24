52c953cc8114bb817df15ca6ca0ef69842681f08
matthias@klein0r.de <Matthias Kleine>
2019-11-24 21:37:00 +0100

    Translations

--- admin/words.js
+++ admin/words.js
@@ -3,39 +3,27 @@
 
 systemDictionary = {
     'trashschedule adapter settings': {
-        'en': 'Adapter settings for trashschedule',
-        'de': 'Adaptereinstellungen für trashschedule',
-        'ru': 'Настройки адаптера для trashschedule',
-        'pt': 'Configurações do adaptador para trashschedule',
-        'nl': 'Adapterinstellingen voor trashschedule',
-        'fr': "Paramètres d'adaptateur pour trashschedule",
-        'it': "Impostazioni dell'adattatore per trashschedule",
-        'es': 'Ajustes del adaptador para trashschedule',
-        'pl': 'Ustawienia adaptera dla trashschedule',
-        'zh-cn': 'trashschedule的适配器设置'
+        'en': 'Adapter settings for Trash-Schedule',
+        'de': 'Adaptereinstellungen für Müll-Planer'
     },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
-        'ru': 'Опция 1',
-        'pt': 'Opção 1',
-        'nl': 'Optie 1',
-        'fr': 'Option 1',
-        'it': 'opzione 1',
-        'es': 'Opción 1',
-        'pl': 'opcja 1',
-        'zh-cn': '选项1'
+    'main settings': {
+        'en': 'Main Settings',
+        'de': 'Haupteinstellungen'
     },
-    'option2': {
-        'en': 'option2',
-        'de': 'Option 2',
-        'ru': 'option2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'zh-cn': '选项2'
+    'trash types': {
+        'en': 'Trash Types',
+        'de': 'Abfall-Typen'
+    },
+    'ical instance': {
+        'en': 'ical instance',
+        'de': 'ical-Instanz'
+    },
+    'name': {
+        'en': 'Name',
+        'de': 'Name'
+    },
+    'event match': {
+        'en': 'Matches events',
+        'de': 'Gilt für Termine'
     }
 };
