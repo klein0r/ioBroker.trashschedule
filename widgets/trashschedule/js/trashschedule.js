2aebeca0f3c4aaf095af625a59ce836e28e6663b
Matthias Kleine <info@haus-automatisierung.com>
2020-03-06 21:29:50 +0100

    use filter to colorize background

--- widgets/trashschedule/js/trashschedule.js
+++ widgets/trashschedule/js/trashschedule.js
@@ -42,6 +42,41 @@ vis.binds['trashschedule'] = {
             });
         }
     },
+    toHSL: function(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+        var r = parseInt(result[1], 16);
+        var g = parseInt(result[2], 16);
+        var b = parseInt(result[3], 16);
+
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if (max == min){
+            h = s = 0; // achromatic
+        } else {
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch(max) {
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+
+        s = s * 100;
+        s = Math.round(s);
+        l = l * 100;
+        l = Math.round(l);
+        h = Math.round(360 * h);
+
+        // Ausgangsfarbe: #00892c, HSL => 139°, 100%, 27%
+        // Nach sepia: #3b3529, HSL => 40°, 18%, 20%
+
+        return 'brightness(50%) sepia(1)  hue-rotate(' + (h - 40) + 'deg) saturate(' + (100 + (s - 18)) + '%) brightness(' + (100 + (l - 20)) + '%)';
+    },
     redraw: function(target, json) {
 
         const dateOptions = { weekday: 'long', month: 'numeric', day: 'numeric' };
@@ -56,11 +91,7 @@ vis.binds['trashschedule'] = {
             $('<span class="nextdate"></span>').html(new Date(trashType.nextdate).toLocaleDateString('de-DE', dateOptions)).appendTo(newItem);
 
             if (trashType._color) {
-                newItem.css(
-                    {
-                        "background-color": "red !important"
-                    }
-                );
+                newItem.find('.dumpster').css('filter', vis.binds['trashschedule'].toHSL(trashType._color));
             }
 
             target.append(newItem);
