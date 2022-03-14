5b477c391f6a8807c7688406c3da44e070da89d3
Matthias Kleine <github.com9@emailu.de>
2022-03-14 10:08:19 +0100

    chore: release v1.4.4
    * (klein0r) Updated dependencies

--- widgets/trashschedule/js/trashschedule.js
+++ widgets/trashschedule/js/trashschedule.js
@@ -146,7 +146,7 @@ $.extend(
 );
 
 vis.binds['trashschedule'] = {
-    version: '1.4.3',
+    version: '1.4.4',
     showVersion: function () {
         if (vis.binds['trashschedule'].version) {
             console.log('Version trashschedule: ' + vis.binds['trashschedule'].version);
